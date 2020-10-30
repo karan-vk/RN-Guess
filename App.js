a51c152d480f342ef745a5c9ad7a3ca5411bceac
Karan <karanvknarayanan@gmail.com>
2020-10-30 23:42:16 +0530

    added header

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import Header from "./components/Header";
 
 export default function App() {
-  return <View></View>;
+  return (
+    <View style={styles.screen}>
+      <Header title="App" />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const StartGameScreen = (props) => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({});
+
+export default StartGameScreen;
